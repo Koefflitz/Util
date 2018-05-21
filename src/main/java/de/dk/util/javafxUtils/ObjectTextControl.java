789141677b9b52e46df827d5f0b8de0cec967578
David <no email>
2018-05-21 14:35:26 +0200

    Some packages moved into their own project
    - command line parser code moved into new clp project
    - multiplexing code moved into new multiplex project
    - fxutils code moved into new fx-utils project

--- src/main/java/de/dk/util/javafxUtils/ObjectTextControl.java
+++
@@ -1,9 +0,0 @@
-package de.dk.util.javafxUtils;
-
-/**
- * @author David Koettlitz
- * <br>Erstellt am 06.12.2016
- */
-public interface ObjectTextControl<O> extends ObjectControl<O, String>, TextControl {
-
-}
