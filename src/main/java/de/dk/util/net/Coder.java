f09b44be7411f27b898bfe53b937ba40abb6cd8c
David <no email>
2018-05-21 14:37:29 +0200

    net package deleted

--- src/main/java/de/dk/util/net/Coder.java
+++
@@ -1,8 +0,0 @@
-package de.dk.util.net;
-
-import java.io.IOException;
-
-public interface Coder {
-   public byte[] encode(byte[] data) throws IOException;
-   public byte[] decode(byte[] data) throws IOException;
-}
